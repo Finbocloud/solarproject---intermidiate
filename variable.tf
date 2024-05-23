30539dab950cffcc0b4da39077963ef66dcb2038
mojilearns <oyedelemojisola@gmail.com>
2024-05-23 19:21:50 +0100

    created secure project

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "owner" {
+  description = "name of secure  project"
+  type = string
+  default = "solar"
+}
+variable "environment" {
+  description = "name of secure environment"
+  type = string
+  default = "secure"
+}
