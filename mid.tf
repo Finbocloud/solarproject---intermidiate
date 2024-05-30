e83c78660893f7a7687eae3c8bdb32bc85e7151c
Eka Amasi <ea22aai@herts.ac.uk>
2024-05-30 21:05:04 +0100

    added files

---
+++ mid.tf
@@ -0,0 +1,6 @@
+#Resource for managed Identity
+resource "azurerm_user_assigned_identity" "example" {
+  location            = azurerm_resource_group.example.location
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+}
---
+++ winvm.tf
@@ -0,0 +1,25 @@
+resource "azurerm_windows_virtual_machine" "this_rg_winvm" {
+  name                = var.win_vm_name
+  resource_group_name = azurerm_resource_group.this_rg.name
+  location            = azurerm_resource_group.this_rg.location
+  size                = "Standard_F2"
+  admin_username      = var.win_vm_username
+  #value for password will be in the KV
+  #and accessed only by managed identity resource
+  admin_password      = var.win_vm_password
+  network_interface_ids = [
+    azurerm_network_interface.this_vm_nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
