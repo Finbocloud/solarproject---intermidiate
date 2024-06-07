2e6b7f679004fde1037910891ecb24535e8d86c5
sargeene <sargeene@gmail.com>
2024-06-07 21:41:23 +0100

    adding keyvault and kvnetworking

---
+++ provider.tf
@@ -0,0 +1,15 @@
+#provider with version - 3.104.0 
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.104.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+provider "random" {
+  # Configuration options
+}
