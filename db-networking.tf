478690efdf97489f71b94d207047060ab4b6b351
sargeene <sargeene@gmail.com>
2024-06-13 20:39:01 +0100

    update

--- db-networking.tf
+++ db-networking.tf
@@ -3,15 +3,15 @@ resource "azurerm_subnet" "this_db_subnet" {
   resource_group_name  = azurerm_resource_group.this_rg.name
   virtual_network_name = azurerm_virtual_network.this_vnet.name
   address_prefixes     = ["10.0.9.0/24"]
-  #delegation {
-  # name = "myDelegation"
-  #service_delegation {
-  #  name = "Microsoft.DBforMySQL/flexibleServers"
-  #  actions = [
-  #   "Microsoft.Network/virtualNetworks/subnets/join/action",
-  # ]
-  #}
-  #}
+  delegation {
+   name = "myDelegation"
+   service_delegation {
+    name = "Microsoft.DBforMySQL/flexibleServers"
+    actions = [
+     "Microsoft.Network/virtualNetworks/subnets/join/action",
+   ]
+  }
+  }
   depends_on = [azurerm_virtual_network.this_vnet]
 }
 resource "azurerm_network_security_group" "this_db_nsg" {
